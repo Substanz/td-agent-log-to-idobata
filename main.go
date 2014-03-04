120c4a83e04736402c4c4ff4e71dd2cb63281a9f
Keiji Matsuzaki <kmatsuzaki@aiming-inc.com>
2014-03-04 15:41:36 +0900

    Modified: Support log-type of td-agent.log.

--- main.go
+++ main.go
@@ -13,12 +13,63 @@ import (
 	"strings"
 )
 
+var (
+	config_load_begin = regexp.MustCompile(`\d{4}-\d\d-\d\d \d\d:\d\d:\d\d \+\d\d\d\d \[info\]: using configuration file: <ROOT>`)
+	config_load_end   = regexp.MustCompile(`</ROOT>`)
+	log_level_pattern = regexp.MustCompile(`\d{4}-\d\d-\d\d \d\d:\d\d:\d\d \+\d\d\d\d \[(.*?)\]: (.*)?`)
+)
+
+const (
+	INITIALIZE_CONFIG = 0
+	ERROR_TYPE        = 1
+	WARN_TYPE         = 2
+	OTHER_TYPE        = 1000
+)
+
 type Setting struct {
 	Idobata string `json:idobata`
 	Td_Log  string `json:td_log`
 }
 
+type Config struct {
+	Message []string
+	Loading bool
+}
+
+func (c Config) messageType(message string) int {
+	p := log_level_pattern.FindStringSubmatch(message)
+
+	if p != nil {
+		if p[1] == "error" {
+			return ERROR_TYPE
+		}
+		if p[1] == "warn" {
+			return WARN_TYPE
+		}
+	}
+
+	if config_load_begin.FindStringSubmatch(message) != nil {
+		return INITIALIZE_CONFIG
+	} else {
+		return OTHER_TYPE
+	}
+}
+
+func (c Config) isInitializeLogEnd(message string) bool {
+	if config_load_end.FindStringSubmatch(message) != nil {
+		c.Loading = false
+		return true
+	} else {
+		return false
+	}
+}
+
+func (c *Config) appendMessage(message string) {
+	c.Message = append(c.Message, message)
+}
+
 func main() {
+	var c Config
 
 	file, e := ioutil.ReadFile("./setting.json")
 	if e != nil {
@@ -26,6 +77,7 @@ func main() {
 	}
 	fmt.Printf("%s\n", file)
 
+	var messageType int
 	var setting Setting
 	json.Unmarshal(file, &setting)
 
@@ -38,32 +90,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	firstMatch, _ := regexp.Compile(`\d{4}-\d\d-\d\d \d\d:\d\d:\d\d \+\d\d\d\d \[info\]: using configuration file: <ROOT>`)
-	lastMatch, _ := regexp.Compile(`</ROOT>`)
-
-	var firstResult []string
-	var lastResult []string
-	var configMessage []string
-	var flag bool
-
 	for line := range t.Lines {
-		firstResult = firstMatch.FindStringSubmatch(line.Text)
-		lastResult = lastMatch.FindStringSubmatch(line.Text)
+		messageType = c.messageType(line.Text)
 
-		if firstResult != nil {
-			flag = true
-			continue
+		if messageType == INITIALIZE_CONFIG {
+			c.Loading = true
 		}
 
-		if lastResult != nil {
-			flag = false
-			postMessage("td-agentがconcatしたtd-agentの設定です: \n"+strings.Join(configMessage, "\n"), setting)
-			configMessage = []string{}
+		if messageType == WARN_TYPE ||
+			messageType == ERROR_TYPE {
+			postMessage(line.Text, setting)
 		}
 
-		if flag == true {
-			configMessage = append(configMessage, line.Text)
+		if c.Loading == true {
+			c.appendMessage(line.Text)
 		}
+
+		if c.isInitializeLogEnd(line.Text) == true {
+			postMessage("[NOW] loading of the td-agent.conf: \n"+strings.Join(c.Message, "\n"), setting)
+			c.Message = []string{}
+		}
+
 	}
 }
 
